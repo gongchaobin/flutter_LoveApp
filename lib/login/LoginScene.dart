ff80ce5024aa5ad7ebc571477cf6b59cd7c8e6f7
龚超斌-13120678 <13120678@suning.com>
2020-08-23 08:41:05 +0800

    登录页面处理

--- lib/login/LoginScene.dart
+++ lib/login/LoginScene.dart
@@ -10,101 +10,104 @@ class LoginScene extends StatefulWidget {
 }
 
 class LoginState extends State<LoginScene> {
+
+  TextEditingController controller = TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    controller.addListener(() {
+
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    controller.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final controller = TextEditingController();
-    controller.addListener(() {
-      print('input ${controller.text}');
-    });
     return Scaffold(
-      appBar: new MyAppBar(
-        centerTitle: '登录',
+      appBar: AppBar(
+        title: Text(
+          "登录"
+        ),
       ),
       body: Container(
         color: Colors.white,
+        padding: EdgeInsets.only(top: 80,left: 20,right: 20),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            Container(
-              margin: EdgeInsets.only(top: 80),
-              child: Image.asset(
-                'images/login/login_logo.png',
-                width: 106,
-                height: 106,
-              ),
+            Image.asset(
+              'images/login/login_logo.png',
+              width: 106,
+              height: 106,
             ),
-            SizedBox(height: 50,),
-            Container(
-              padding: EdgeInsets.only(left: 20,right: 20),
-              child: TextField(
-                controller: controller,
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration.collapsed( 
-                  hintText: '请输入账号',
+            SizedBox(height: 30,),
+            TextField(
+              controller: controller,
+              autofocus: true,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                hintText: "请输入账号",
+                hintStyle: TextStyle(
+                  color: Colors.grey,
                 ),
-                onChanged: (text) {
-                  print('change $text');
-                },
-                onSubmitted: (text) {
-                  print('submit $text');
-                },
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey,
+                    width: 0.5
+                  )
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.blue,
+                    width: 0.5
+                  )
+                )
               ),
+              onChanged: (text) {
+                print('change $text');
+              },
+              onSubmitted: (text) {
+                print('submit $text');
+              },
             ),
-            lineWidget(),
-            SizedBox(height: 5,),
-            Container(
-              padding: EdgeInsets.only(left: 20,right: 20),
-              child: TextField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration.collapsed(
-                  hintText: '请输入密码',
-                  ),
-                onChanged: (text) {
-                  print('change $text');
-                },
-                onSubmitted: (text) {
-                  print('submit $text');
-                },
+            SizedBox(height: 15,),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                hintText: '请输入密码',
+                hintStyle: TextStyle(
+                  color: Colors.grey
+                ),
+                enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                        color: Colors.grey,
+                        width: 0.5
+                    )
+                ),
+                focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                        color: Colors.blue,
+                        width: 0.5
+                    )
+                )
               ),
+              onChanged: (text) {
+                print('change $text');
+              },
+              onSubmitted: (text) {
+                print('submit $text');
+              },
             ),
-            lineWidget(),
-            // SizedBox(height: 50,),
-            // TextField(
-              
-            // ),
-            
-            // SizedBox(height: 5,),
-            // TextField(
-              
-            // ),
-            // lineWidget(),
-            // SizedBox(height: 30,),
-            // FlatButton(
-            //   child: Container(
-            //     margin: EdgeInsets.only(left: 15,right: 15),
-            //     height: 40,
-            //     decoration: BoxDecoration(
-            //       color: Color.fromRGBO(254, 84, 158, 1),
-            //       borderRadius: BorderRadius.all(Radius.circular(10)),
-            //     ),
-            //     child:  Center(
-            //       child: Text('登录'),
-            //     ),
-            //   ), onPressed: () {
-            //     print('登录点击');
-            //   },
-            // ),
-            // SizedBox(height: 20,),
-            
           ],
         ),
       ),
-    );
-  }
-  Widget lineWidget() {
-    return Padding(
-      padding: EdgeInsets.only(left: 15,right: 15),
-      child: Image.asset("images/login/login_sepline.png"),
     );
   }
 }
